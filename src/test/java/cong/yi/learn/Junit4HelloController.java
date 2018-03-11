a0e51e4a99b90930f57c5393153dce889cf69b79
congyilove <15600070591@163.com>
2018-03-11 12:22:26 +0800

    first commit

---
+++ src/test/java/cong/yi/learn/Junit4HelloController.java
@@ -0,0 +1,40 @@
+package cong.yi.learn;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import cong.yi.learn.web.HelloController;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = MockServletContext.class)
+@WebAppConfiguration // 注解表明这里需要初始化一个webAppicationContext
+public class Junit4HelloController {
+
+	private MockMvc mvc;
+
+	@Before
+	public void setUp() throws Exception {
+		mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+	}
+
+	@Test
+	public void getHello() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("hello world")));
+	}
+
+}
